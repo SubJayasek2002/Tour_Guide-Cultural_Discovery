2309cfc12d5e2cb009eb475ba08f8942fcb2a316
SubJayasek2002 <sewminisubanya@gmail.com>
2025-12-12 08:11:50 +0530

    clear project

--- src/main/java/com/tourguide/reviewservice/controller/RatingController.java
+++
@@ -1,5 +0,0 @@
-package com.tourguide.reviewservice.controller;
-
-public class RatingController {
-
-}
--- src/main/java/com/tourguide/reviewservice/service/RatingService.java
+++
@@ -1,5 +0,0 @@
-package com.tourguide.reviewservice.service;
-
-public interface RatingService {
-
-}
