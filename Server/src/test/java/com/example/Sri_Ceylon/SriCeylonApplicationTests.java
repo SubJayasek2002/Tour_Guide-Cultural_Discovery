47fde0e9b85f1fe5ddea8606c9fda07abec8aa6d
SubJayasek2002 <sewminisubanya@gmail.com>
2025-12-12 08:16:48 +0530

    test file

---
+++ Server/src/test/java/com/example/Sri_Ceylon/SriCeylonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Sri_Ceylon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SriCeylonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
